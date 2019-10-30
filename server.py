8acca39f4a0a1112284388b71a4b69a46a267c99
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-30 13:08:45 -0700

    Working on user info page

--- server.py
+++ server.py
@@ -22,7 +22,10 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
+   
     return render_template('homepage.html')
+    
+
 
 @app.route('/users')
 def user_list():
@@ -33,22 +36,62 @@ def user_list():
 def register_form():
     return render_template("registration.html")
 
-@app.route('/register', methods = ["POST"])
+
+@app.route('/register_user', methods = ["POST"])
 def register_user():
-    email = request.form('email')
-    password = request.form('password')
-    age = request.form('age')
-    zipcode = request.form('zipcode')
-    new_user = User(email=email,
-                    password=password,
-                    age=age,
-                    zipcode=zipcode)
-    db.session.add(new_user)
-    db.session.commit()
-    
+    email = request.form.get('email')
+    password = request.form.get('password')
+    age = request.form.get('age')
+    zipcode = request.form.get('zipcode')
+    is_avial = User.query.filter_by(email=email).first()
+    if is_avial is None:
+
+        new_user = User(email=email,
+                        password=password,
+                        age=age,
+                        zipcode=zipcode)
+        db.session.add(new_user)
+        db.session.commit()
+    else:
+        flash('THis user already exists')
+        return redirect('/register')
+
     return redirect('/')
 
 
+@app.route('/loginPage')
+def log_in_page():
+    return render_template('login.html')
+
+
+@app.route('/login')
+def log_in():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    is_avial = User.query.filter_by(email=email).first()
+    print(is_avial.password)
+
+    if (is_avial is None) or (is_avial.password != password):
+        flash('Please enter correct email id or password.')
+        return redirect('/loginPage')
+    else:
+        flash('You are successfully logged in.....')
+        session['id'] = email
+
+        return redirect('/userpage')
+
+
+@app.route('/userpage')
+def userpage():
+    user= User.query.filter_by(email = session.get('id'))
+    return render_template('userPage.html',record=user)
+
+    
+@app.route('/logout')
+def logged_out():
+    flash('You are logged out')
+    session.pop('id')
+    return redirect('/')
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
